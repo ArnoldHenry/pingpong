b6db7f46ae73240a0b55b3ee767dc9846354dfd5
Arnold Henry <arnoldomwoyo2@gmail.com>
2018-05-04 17:41:10 +0300

    added new changes

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+
+function mypingfunction(myresult){
+  var storedarray = [];
+
+  for (var i = 1; i <= myresult; i++) {
+    if ((i%3) == 0) {
+      storedarray.push("ping");
+    }else if ((i%5) == 0 ){
+      storedarray.push("pong");
+    }else if ((i%15) == 0) {
+      storedarray.push("pingpong");
+    }else {
+      storedarray.push(i);
+    }
+  }
+// alert(storedarray);
+return storedarray;
+}
+
+
+$(document).ready(function(){
+  $("form#myformid").submit(function(event){
+       // event.preventDefault();
+    $("#answers").empty();
+    var myresult = parseInt($("input#mynumber").val());
+     var storedarray = mypingfunction(myresult);
+     storedarray.forEach(function(myouttput){
+       $("#answers").append(myouttput + "</br>");
+       event.preventDefault();
+     });
+
+  });
+});
