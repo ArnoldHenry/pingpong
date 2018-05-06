1dd83e5e122f2e06cdf6136ff93f5369d657c05e
ArnoldHenry <arnoldomwoyo2@gmail.com>
2018-05-06 12:37:08 +0300

    made changes to js file-- clear textbox

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,11 @@ function mypingfunction(myresult){
   var storedarray = [];
 
   for (var i = 1; i <= myresult; i++) {
-    if ((i%3) == 0) {
+    if ((i%3) === 0) {
       storedarray.push("ping");
-    }else if ((i%5) == 0 ){
+    }else if ((i%5) === 0 ){
       storedarray.push("pong");
-    }else if ((i%15) == 0) {
+    }else if ((i%15) === 0) {
       storedarray.push("pingpong");
     }else {
       storedarray.push(i);
@@ -20,8 +20,6 @@ return storedarray;
 
 $(document).ready(function(){
   $("form#myformid").submit(function(event){
-       // event.preventDefault();
-
     $("#answers").empty();
     var myresult = parseInt($("input#mynumber").val());
      var storedarray = mypingfunction(myresult);
